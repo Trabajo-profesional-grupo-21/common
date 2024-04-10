49a78b4f87fe29040ec8601b681b4710c53fd21b
SaFernandezC <SFernandezC@fi.uba.ar>
2024-04-10 19:49:19 -0300

    feat: inital setup for commons package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='g21-common',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pika'
+    ],
+    author='TPP - G21',
+    author_email='asegura@fi.uba.ar, msfontenla@fi.uba.ar, iiragui@fi.uba.ar, sfernandezc@fi.uba.ar',
+    description='Modulos compartidos por nuestras apps',
+    url='https://github.com/Trabajo-profesional-grupo-21/common',
+)
